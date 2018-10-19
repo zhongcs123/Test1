5b0c440bcfaf22dcbf8ec213eedac3ba9657f066
zhongcs123 <44253288+zhongcs123@users.noreply.github.com>
2018-10-19 14:53:37 +0800

    Update GitTest.java

--- src/com/git/GitTest.java
+++ src/com/git/GitTest.java
@@ -2,13 +2,14 @@ package com.git;
 
 public class GitTest {
 	public static void main(String[] args) {
-		System.out.println("git²âÊÔ");
-		System.out.println("git²âÊÔ2");
-		System.out.println("git²âÊÔ3");
-		System.out.println("git����4");
-		System.out.println("git����5");
-		System.out.println("git����6");
-		System.out.println("git����7");
-		System.out.println("git����8");
+		System.out.println("gitÂ²Ã¢ÃŠÃ”");
+		System.out.println("gitÂ²Ã¢ÃŠÃ”2");
+		System.out.println("gitÂ²Ã¢ÃŠÃ”3");
+		System.out.println("git²âÊÔ4");
+		System.out.println("git²âÊÔ5");
+		System.out.println("git²âÊÔ6");
+		System.out.println("git²âÊÔ7");
+		System.out.println("git²âÊÔ8");
+		System.out.println("git²âÊÔ9");
 	}
 }
