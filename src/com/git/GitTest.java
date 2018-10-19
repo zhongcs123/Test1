b1e499ab38fbf46edd9b916e9688b3b63f33ad03
zhongcs123 <947426700@qq.com>
2018-10-19 15:58:50 +0800

    测试10

--- src/com/git/GitTest.java
+++ src/com/git/GitTest.java
@@ -1,15 +1,8 @@
 package com.git;
 
+
 public class GitTest {
 	public static void main(String[] args) {
-		System.out.println("gitÂ²Ã¢ÃŠÃ”");
-		System.out.println("gitÂ²Ã¢ÃŠÃ”2");
-		System.out.println("gitÂ²Ã¢ÃŠÃ”3");
-		System.out.println("git²âÊÔ4");
-		System.out.println("git²âÊÔ5");
-		System.out.println("git²âÊÔ6");
-		System.out.println("git²âÊÔ7");
-		System.out.println("git²âÊÔ8");
-		System.out.println("git²âÊÔ9");
+		System.out.println("测试9");
 	}
 }
